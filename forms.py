2ac356c248a483db01665bdeb16c242686d0f5ea
Ailin <ailincaranza@gmail.com>
2019-11-14 00:54:52 -0300

    + Added project files.

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required
+import csv
+
+
+class LoginForm(FlaskForm):
+    usuario = StringField('Nombre de usuario', validators=[Required()])
+    password = PasswordField('Contraseña', validators=[Required()])
+    enviar = SubmitField('Ingresar')
+
+class SaludarForm(FlaskForm):
+    usuario = StringField('Nombre: ', validators=[Required()])
+    enviar = SubmitField('Saludar')
+
+class RegistrarForm(LoginForm):
+    password_check = PasswordField('Verificar Contraseña', validators=[Required()])
+    enviar = SubmitField('Registrarse')
